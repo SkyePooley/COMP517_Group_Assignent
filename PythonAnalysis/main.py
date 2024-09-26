5adc267ffcf9db13b834c16ec02e1d9e6ad50f24
Skye Pooley <tpb6375@autuni.ac.nz>
2024-09-26 16:50:37 +1200

    Created template PyCharm project

---
+++ PythonAnalysis/main.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+DATA_FILEPATH = "Employee_Performance.csv"
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+def clean_data(dataframe):
+    # TODO check dataset for missing values, outliers, and duplicates. Return clean copy.
+    return dataframe
+
+
+if __name__ == '__main__':
+    rawDataframe = load_data(DATA_FILEPATH)
+    cleanDataframe = clean_data(rawDataframe)
+    print(cleanDataframe.head())
+    # TODO create and call methods to generate graphs and calculate statistics.
+    # Avoid giving a method multiple jobs
+
